0ae52e71e175a4e9df9087b03e3be90a4412e01a
rraaffly <muh.raflyrafsanjani@gmail.com>
2025-08-15 09:32:58 +0700

    test files

---
+++ tests/sample.test.ts
@@ -0,0 +1,17 @@
+import { expect, test } from "bun:test";
+
+test("Penjumlahan", () => {
+  expect(2 + 9).toBe(11);
+});
+
+test("Pengurangan", () => {
+  expect(7 - 2).toBe(5);
+});
+
+test("Pembagian", () => {
+  expect(3 / 3).toBe(1);
+});
+
+test("Perkalian", () => {
+  expect(2 * 2).toBe(4);
+});
